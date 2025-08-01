baec7ba05189563977065c09aaeb30b519502db5
PY0226H <pyh20010226@gmail.com>
2025-08-01 02:20:32 -0700

    feature: allow send message and process sse

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './assets/style.css'; // Import any global styles
+
+import './assets/styles.css'; // Import any global styles
 
 const app = createApp(App);
+
+// Load user state from localStorage when the app starts
+store.dispatch('loadUserState');
 
 app.use(store);
 app.use(router);
